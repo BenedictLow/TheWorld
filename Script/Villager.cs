1924ec72eb6ad9d9ff90fee7c76bfab96011ded3
BenedictLow <low_ben@rocketmail.com>
2016-03-08 19:47:46 +0800

    Where I am now
    Where you wont be

---
+++ Script/Villager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Villager : MonoBehaviour {
+
+	private NavMeshAgent nav;
+	public float speed;
+	public Transform[] waypoints;
+	private int pointer = 0;
+	private int maxWaypoint;
+	public float minWaypointDistance = 0.1f;
+	// Use this for initialization
+	void Awake () 
+	{
+		nav = GetComponent<NavMeshAgent> ();
+
+		maxWaypoint = waypoints.Length - 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Travel ();
+	}
+	void Travel()
+	{
+		nav.speed = speed;
+
+		Vector3 travelFrom;
+		Vector3 travelTo;
+
+		travelFrom = transform.position;
+		travelFrom.y = 0.0f;
+
+		travelTo = waypoints [pointer].position;
+		if (Vector3.Distance (travelTo, travelFrom) < minWaypointDistance) 
+		{
+			if (pointer == maxWaypoint) {
+				pointer = 0;
+			} 
+			else
+			{
+				pointer++;
+			}
+		}
+
+		nav.SetDestination (travelTo);
+	}
+}
---
+++ Script/VillagerAI/ClassesStateBase.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassesStateBase : MonoBehaviour {
+
+	public virtual void Enter()
+	{
+	}
+
+	public virtual void Execute()
+	{}
+
+	public virtual void FixedExecute()
+	{}
+
+	public virtual void End()
+	{}
+}
